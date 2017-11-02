126d3fdb5a54567a46e76fa33180825dd7ecd98c
PedroL7 <32001412+PedroL7@users.noreply.github.com>
2017-11-02 19:46:25 +0000

    Add files via upload

---
+++ Week7Ex1,3.py
@@ -0,0 +1,6 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:\Users\pedro\Desktop\test.py ==================
+[66.25, 333, 1, 1234.5]
+>>> 
---
+++ Week7Ex2.py
@@ -0,0 +1,5 @@
+squares = []
+for i in range(11):
+    newsquare = i * i
+    squares.append(newsquare)
+print(squares)
