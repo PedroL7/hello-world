ad25b86d99789887ad06c4f96b9104105ff38f3b
PedroL7 <32001412+PedroL7@users.noreply.github.com>
2017-12-11 14:10:54 +0000

    Delete Week7Ex1,4.py

--- Week7Ex1,4.py
+++
@@ -1,6 +0,0 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-================== RESTART: C:\Users\pedro\Desktop\test.py ==================
-[1234.5, 1, 333, 333, 66.25]
->>> 
